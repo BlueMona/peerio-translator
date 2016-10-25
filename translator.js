61521f45a91e536516615ae52c76884be4240b6f
Anri Asaturov <anri82@gmail.com>
2016-10-25 21:05:21 +0300

    Revert "WIP: add observable to t"
    This reverts commit be36c27cc8cfd1c5b656b934c72afcb45348c3b6.

--- translator.js
+++ translator.js
@@ -1,10 +1,4 @@
 /* eslint-disable guard-for-in, no-prototype-builtins, no-param-reassign, no-confusing-arrow */
-import { observable } from 'mobx';
-
-const config = observable({
-    locale: null
-});
-
 let translation = {};
 let locale = null;
 
@@ -13,7 +7,6 @@ function setLocale(newLocale, newTranslation) {
         return;
     }
     locale = newLocale;
-    config.locale = newLocale;
     translation = newTranslation;
     compileTranslation();
 }
@@ -23,10 +16,6 @@ function has(id) {
 }
 
 function t(id, params) {
-    /* hacky way to make t observable */
-    if (config.locale !== locale) {
-        console.error('locale mismatch');
-    }
     let ret = translation[id] || id;
 
     // this is segmented string
