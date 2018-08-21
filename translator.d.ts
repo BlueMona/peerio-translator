4ffd87454ddba78aa1d075056d523761313ff0e2
Anri Asaturov <anri82@gmail.com>
2018-08-21 21:43:03 +0300

    types

---
+++ translator.d.ts
@@ -0,0 +1,14 @@
+type VarParam = { toString: () => string };
+type TemplateParam = (text: string) => any;
+type Params = { [key: string]: VarParam | TemplateParam };
+type Result = string | Array<any>;
+
+export module 'peerio-translator' {
+  export function setLocale(locale: string, translation: { [key: string]: string }): void;
+  export function t(id: string, params?: Params): Result;
+  export function tu(id: string, params?: Params): Result;
+  export function has(id: string): boolean;
+  export function setStringReplacement(pattern: string, replacement: string): void;
+  export function setTagHandler(tag: string, handler: TemplateParam): void;
+  export function setUrlMap(map: { [key: string]: string }): void;
+}
